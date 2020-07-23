2a98ee46be2169f2ba8531843a0490581873d404
viralsir <viralsir2018@gmail.com>
2020-07-23 18:28:16 +0530

    add maximum number solutions

--- firstdemo.py
+++ firstdemo.py
@@ -1,2 +1,12 @@
 print("hello")
 print("Wel come to the world of github")
+
+no1=int(input("Enter No1:"))
+no2=int(input("Enter No2:"))
+
+if no1>no2:
+    print(no1,"is a maximum no")
+else :
+    print(no2,"is a maximum no")
+
+
