0d3970fdfdb08990c520ca22a07f7d7ccb8f4903
viralsir <viralsir2018@gmail.com>
2020-07-23 18:51:33 +0530

    Revert "add factorial in even odd file"
    This reverts commit ea254a03

--- even_odd.py
+++ even_odd.py
@@ -2,10 +2,4 @@ no=int(input("Enter No:"))
 if no%2==0:
     print("given no is even ")
 else :
-    print("given no  is odd")
-    
-fact=1
-for i in range(1,no+1,1):
-    fact=fact*i
-    
-print("factorial :",fact);
+    print("no is odd")
