389112f010440f0e133c9399a3bdb457dbfe59fa
viralsir <viralsir2018@gmail.com>
2020-07-23 18:52:18 +0530

    Revert "Update even_odd.py"
    This reverts commit 4c9770cb

--- even_odd.py
+++ even_odd.py
@@ -2,4 +2,10 @@ no=int(input("Enter No:"))
 if no%2==0:
     print("given no is even ")
 else :
-    print("no is odd")
+    print("given no  is odd")
+    
+fact=1
+for i in range(1,no+1,1):
+    fact=fact*i
+    
+print("factorial :",fact);
