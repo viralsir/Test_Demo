4c9770cbabc4109409aa0744664248fd544c2bd5
viralsir <53177289+viralsir@users.noreply.github.com>
2020-07-23 18:48:51 +0530

    Update even_odd.py

--- even_odd.py
+++ even_odd.py
@@ -1,8 +1,8 @@
 no=int(input("Enter No:"))
 if no%2==0:
-    print("no is even ")
+    print("given no is even ")
 else :
-    print("no is odd")
+    print("given no  is odd")
     
 fact=1
 for i in range(1,no+1,1):
