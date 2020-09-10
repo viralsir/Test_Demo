3d97d089dcb7a0bf5b3eda3226d774f8196f6744
viralsir <viralsir2018@gmail.com>
2020-09-10 22:53:13 +0530

    added html and css properties to code

--- even_odd.py
+++ even_odd.py
@@ -3,9 +3,9 @@ if no%2==0:
     print("given no is even ")
 else :
     print("given no  is odd")
-    
-fact=1
-for i in range(1,no+1,1):
-    fact=fact*i
-    
-print("factorial :",fact);
+
+fact = 1
+for i in range(1, no + 1, 1):
+    fact = fact * i
+
+print("factorial :", fact);
